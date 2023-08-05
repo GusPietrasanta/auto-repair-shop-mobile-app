dcc7c68bcdfdad00e9f9ddb6c9ef9460e92ccebf
GusPietrasanta <gusdsm@gmail.com>
2023-08-05 17:35:24 +0930

    Updated API URL

--- App/App/Models/Session.cs
+++ App/App/Models/Session.cs
@@ -2,7 +2,7 @@
 
 public static class Session
 {
-    public static string BaseUrl { get; } = "http://192.168.1.100:45500/api";
+    public static string BaseUrl { get; } = "http://ec2-13-239-17-109.ap-southeast-2.compute.amazonaws.com:8000/api";
     
     public static DetailedAppointment CurrentAppointment { get; set; }
 
